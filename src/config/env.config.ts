88665bd3984b4a3f6a1dcd1b03015866c909bf72
NestorDanielGomez <nestordanielgomez@gmail.com>
2023-10-06 14:57:19 -0300

    correccion default limit

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,5 +2,5 @@ export const EnvConfiguration = () => ({
     enviroment: process.env.NODE_ENV || "dev",
     mongodb: process.env.MONGO_DB,
     port: process.env.PORT || 3001,
-    default_limit: process.env.DEFAULT_LIMIT || 7
+    default_limit: +process.env.DEFAULT_LIMIT || 7
 })
--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -3,5 +3,5 @@ import * as Joi from "joi";
 export const JoinValidationSchema = Joi.object({
     MONGO_DB: Joi.required(),
     PORT: Joi.number().default(3005),
-    DEFAULT_LIMIT: +Joi.number().default(6)
+    DEFAULT_LIMIT: Joi.number().default(6)
 })
